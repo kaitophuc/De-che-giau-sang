54a60228b24de8de44a325461b7901347655f79d
HungLuong05 <luongduc@msu.edu>
2024-09-15 10:23:03 -0400

    calendar

--- frontend/src/components/Calendar/WeekView/WeekView.tsx
+++ frontend/src/components/Calendar/WeekView/WeekView.tsx
@@ -1,32 +1,50 @@
 import { useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faClock } from '@fortawesome/free-solid-svg-icons';
+import { faClock } from '@fortawesome/free-regular-svg-icons';
+
+import Day from './Day.tsx';
 
 import styles from './WeekView.module.css';
 
 const WeekView = () => {
   useEffect(() => {
     
-  }, [])
+  }, []);
+
+  const hourBoxes = [];
+  const hourLines = [];
+  for (let i = 0; i < 24; i++){
+    const hour = i >= 12 ? `${i > 12 ? i - 12 : i} PM` : `${i == 0 ? 12 : i} AM`;
+    hourBoxes.push(
+      <div className={styles.hourBox} key={i}>
+        {hour}
+      </div>
+    )
+  }
+
+  for (let i = 0; i < 25; i++){
+    const hourLinePosition = `${i * 50}px`;
+    hourLines.push(
+      <div className={styles.hourLine}
+        style={{top: hourLinePosition}}
+        key={i}
+      ></div>
+    )
+  }
 
   return (
-    <div className={styles.grid_container}>
-      <FontAwesomeIcon icon={faClock}/>
-      <h1>Monday</h1>
-      <h1>Tuesday</h1>
-      <h1>Wednesday</h1>
-      <h1>Thursday</h1>
-      <h1>Friday</h1>
-      <h1>Saturday</h1>
-      <h1>Sunday</h1>
-      <div></div>
-      <div className={styles.day}></div>
-      <div className={styles.day}></div>
-      <div className={styles.day}></div>
-      <div className={styles.day}></div>
-      <div className={styles.day}></div>
-      <div className={styles.day}></div>
-      <div className={styles.day}></div>
+    <div className={styles.calendar_container}>
+      {hourLines}
+      <div className={styles.calendar_sideTime}>{hourBoxes}</div>
+      <div className={styles.calendar_main}>
+        <Day name="Monday"/>
+        <Day name="Tuesday"/>
+        <Day name="Wednesday"/>
+        <Day name="Thursday"/>
+        <Day name="Friday"/>
+        <Day name="Saturday"/>
+        <Day name="Sunday"/>
+      </div>
     </div>
   );
 }
