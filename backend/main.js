02e31f59899ea3d07290f66ba7e72466fdbdb403
Minh Phuc Nguyen <nguyenp@union.edu>
2024-09-15 10:19:53 -0400

    hung ham

--- backend/main.js
+++ backend/main.js
@@ -1,13 +1,29 @@
-import express from "express";
-import cors from "cors";
+// import express from "express";
+// import cors from "cors";
 
-const PORT = process.env.PORT || 5050;
+// const PORT = process.env.PORT || 5050;
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+// app.use("/", greet);
+
+// app.listen(PORT, () => {
+//   console.log(`Server listening on port ${PORT}`);
+// });
+
+import express from 'express';
+
 const app = express();
 
-app.use(cors());
 app.use(express.json());
-app.use("/", greet);
 
-app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+
+app.post('/products');
+
+app.get('/products');
+
+
+app.listen(5050, () => {
+  console.log('Server listening on port 5050');
 });
