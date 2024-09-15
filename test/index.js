0d371d3d4278130e12c289b41e3602940cac068a
HungLuong05 <luongduc@msu.edu>
2024-09-15 11:07:52 -0400

    add save function

--- test/index.js
+++ test/index.js
@@ -3,18 +3,40 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-mongoose.connect('mongodb+srv://phucking:N4a6pkS9QmWz0wnS@cluster0.0bgtfdd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/Calendar_service');
+mongoose.connect('mongodb+srv://phucking:N4a6pkS9QmWz0wnS@cluster0.0bgtfdd.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(() => {
+    console.log('Connected to database');
+});
 
 const UserSchema = new mongoose.Schema({
     date: Date,
-    description: String
+    description: String,
 });
 
-const Events = mongoose.model('event', UserSchema);
-
 app.get('/Calendar_service', (req, res) => {
-    Events.find({}).then(function(event){
+    const Events = mongoose.model('Calendar', UserSchema);
+
+    Events.find({}).then((event) => {
         res.json(event);
+        console.log(event);
+    }).catch(function(err){
+        console.log(err);
+    });
+});
+
+app.get('/new_event', (req, res) => {
+    const Events = mongoose.model('Calendar', UserSchema);
+
+    const sample_event = new Events({
+        date: '',
+        description: 'Sample event',
+    });
+    // const event = new Events({
+    //     date: req.body.date,
+    //     description: req.body.description,
+    // });
+    sample_event.save().then((event) => {
+        res.json(event);
+        console.log("What the fuck???");
         console.log(event);
     }).catch(function(err){
         console.log(err);
