3f3d716350f8f5ec108465a565ced738ac92ba8c
Minh Phuc Nguyen <nguyenp@union.edu>
2024-09-15 18:47:32 -0400

    query from url

--- test/index.js
+++ test/index.js
@@ -4,7 +4,8 @@ const mongoose = require('mongoose');
 const app = express();
 
 mongoose.connect('mongodb+srv://phucking:N4a6pkS9QmWz0wnS@cluster0.0bgtfdd.mongodb.net/Calendar_service?retryWrites=true&w=majority&appName=Cluster0').then(() => {
-    console.log('Connected to database');
+    //console.log('Connected to database');
+    console.log(`Connected to database: ${mongoose.connection.name}`);
 });
 
 const UserSchema = new mongoose.Schema({
@@ -12,7 +13,7 @@ const UserSchema = new mongoose.Schema({
     description: String,
 });
 
-const Events = mongoose.model('Nani', UserSchema);
+const Events = mongoose.model('Calendar', UserSchema);
 
 app.get('/Calendar_service', (req, res) => {
     Events.find({}).then((event) => {
@@ -24,17 +25,28 @@ app.get('/Calendar_service', (req, res) => {
 });
 
 app.get('/new_event', (req, res) => {
-    const sample_event = new Events({
-        date: '',
-        description: 'Sample event',
+    // Access the query parameters from the URL
+    const { date, description } = req.query;
+
+    // Create a new event instance
+    const newEvent = new Events({
+        date: new Date(date),
+        description: description
     });
-    // const event = new Events({
-    //     date: req.body.date,
-    //     description: req.body.description,
-    // });
-    sample_event.save().then((event) => {
+
+    // Save the new event to the database
+    newEvent.save().then((event) => {
         res.json(event);
-        console.log("What the fuck???");
+        console.log(event);
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).send('Error saving event');
+    });
+});
+
+app.get('/delete_event', (req, res) => {
+    Events.deleteMany({}).then((event) => {
+        res.json(event);
         console.log(event);
     }).catch(function(err){
         console.log(err);
