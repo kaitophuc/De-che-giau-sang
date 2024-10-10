1f8cae745529bfcbb0ebacd19a0befeedc033e2b
Minh Phuc Nguyen <nguyenp@union.edu>
2024-10-09 21:09:07 -0400

    index

--- test/index.js
+++ test/index.js
@@ -24,6 +24,7 @@ app.get('/Calendar_service', (req, res) => {
     });
 });
 
+//http://localhost:5050/new_event?date=2024-09-15&description=This%20is%20a%20test%20description
 app.get('/new_event', (req, res) => {
     // Access the query parameters from the URL
     const { date, description } = req.query;
@@ -45,13 +46,24 @@ app.get('/new_event', (req, res) => {
 });
 
 app.get('/delete_event', (req, res) => {
-    Events.deleteMany({}).then((event) => {
+    // Access the query parameters from the URL
+    const { date, description } = req.query;
+
+    // Create a new event instance
+    const newEvent = new Events({
+        date: new Date(date),
+        description: description
+    });
+
+    // Delete the new event from the database
+    Events.deleteMany({date: new Date(date), description: description}).then((event) => {
         res.json(event);
         console.log(event);
     }).catch(function(err){
         console.log(err);
     });
 });
+
 
 app.listen(5050, () => {
   console.log('Server is running on port 5050');
