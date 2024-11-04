f8ad402816292074c2fab0ae1b5a40391521b058
HungLuong05 <152527105+HungLuong05@users.noreply.github.com>
2024-11-03 22:18:27 -0500

    Features/authorization (#7)
    * Authorization

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,10 +1,10 @@
-const { login, logout, signUp, update } = require('../controllers/user/auth');
+// const { login, logout, signUp, update } = require('../controllers/user/auth');
 
-const authRouter = require('express').Router();
+// const authRouter = require('express').Router();
 
-authRouter.post('/signup', signUp);
-authRouter.post('/login', login);
-authRouter.post('/logout', logout);
-authRouter.post('/update', update);
+// authRouter.post('/signup', signUp);
+// authRouter.post('/login', login);
+// authRouter.post('/logout', logout);
+// authRouter.post('/update', update);
 
-module.exports = authRouter;
+// module.exports = authRouter;
