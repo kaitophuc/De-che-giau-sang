6acb8940af936ac79b90546bd95055ef4066b92e
HungLuong05 <152527105+HungLuong05@users.noreply.github.com>
2024-11-03 22:18:27 -0500

    Features/authorization (#7)
    * Authorization

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,37 @@
+import { useMemo, useState } from 'react';
 import { useRoutes } from 'react-router-dom';
 
 import Calendar from './pages/Calendar/Calendar.tsx';
 import Login from './pages/Auth/Login.tsx';
+import Register from './pages/Auth/Register.tsx';
+import ProtectedRoute from './utils/ProtectedRoute/ProtectedRoute.tsx';
+import { AuthContext } from './pages/Auth/context/AuthContext.tsx';
+import { User } from './pages/Auth/hooks/useUser.tsx';
+import { useAuth } from './pages/Auth/hooks/useAuth.tsx';
 
 const App: React.FC = () => {
+  const [user, setUser] = useState<User | null>(null);
+  // const { user, login, logout, setUser } = useAuth();
+
   const router = useRoutes([
     {
       path: '/',
-      element: <Calendar />,
+      element: <ProtectedRoute element={<Calendar />} />,
     },
     {
       path: '/login',
       element: <Login />,
+    },
+    {
+      path: '/register',
+      element: <Register />,
     }
   ])
 
   return (
-    <>
+    <AuthContext.Provider value={useMemo(() => ({ user, setUser }), [user, setUser])}>
       {router}
-    </>
+    </AuthContext.Provider>
   );
 };
 
--- frontend/src/pages/Auth/context/AuthContext.tsx
+++ frontend/src/pages/Auth/context/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { createContext } from 'react';
+import { createContext, useState, useMemo } from 'react';
 import { User } from '../hooks/useUser';
 
 interface AuthContext {
